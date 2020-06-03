c6de604f5e1f354af2dc61fd6894a42553c5851e
zartre <paulpreuss@gmail.com>
2020-06-03 15:05:57 +0200

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+//Main ist die "primary launch class" und extended "Application"
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //ein Parent ist eine branch node
+        //dieses Parent ist dabei auch eine layout component
+        //dieses Parent ist die Scene für meine Stage!
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //label ist text innerhalb des fensters
+        Label label = new Label("LABELTEXT");
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        //Stage secondaryStage = new Stage();
+        //secondaryStage.setScene(new Scene(label, 400, 500));
+        //secondaryStage.show();
+        //secondaryStage.setFullScreen(true);
+    }
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
